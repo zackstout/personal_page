c11fa86b2cbce055f1d43bf3ce0ca1b925fe7408
Zack Stout <zackstout@gmail.com>
2017-12-06 19:10:29 -0600

    jeez took a long time to find that res.sendFile function in the indexRouter of base project

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,2 +1,4 @@
 
 console.log('we in');
+
+var app = angular.module('app', ['ngRoute']);
--- server/server.js
+++ server/server.js
@@ -1,13 +1,27 @@
 
 var express = require('express');
 var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+// console.log(app);
+
+var port = process.env.PORT || 6660;
 
 
-var port = process.env.PORT || 5454;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Serve back static files
 app.use(express.static('./server/public'));
 
+app.get('/', function(req, res) {
+  console.log('request for index');
+  res.sendFile(path.join(__dirname, './public/views/index.html'));
+});
+
+// app.get('/', function(req, res){
+//   res.sendStatus(200);
+// });
 
 // Listen //
 app.listen(port, function(){
