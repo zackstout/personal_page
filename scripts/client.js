c669d18c1f879a926240e67ee7e1247552bc6ce5
Zack Stout <zackstout@gmail.com>
2017-12-18 15:30:53 -0600

    added wraparound server

--- scripts/client.js
+++ scripts/client.js
@@ -7,15 +7,15 @@ app.config(function($routeProvider, $locationProvider) {
   $locationProvider.hashPrefix('');
   console.log('myApp -- config');
   $routeProvider.when('/home', {
-    templateUrl: '/views/templates/about.html',
+    templateUrl: 'views/templates/about.html',
     controller: 'AboutController as ac',
   })
   .when('/blog', {
-    templateUrl: '/views/templates/blog.html',
+    templateUrl: 'views/templates/blog.html',
     controller: 'BlogController as bc'
   })
   .when('/portfolio', {
-    templateUrl: '/views/templates/portfolio.html',
+    templateUrl: 'views/templates/portfolio.html',
     controller: 'PortfolioController as pc'
 
   })
